392a168f245dc7afa16431f3efd77b355e5199bb
Jefferson gomes <jefferson.silva@medral.com.br>
2022-05-02 22:03:25 -0300

    Commit like deslike complete

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,6 +1,7 @@
 const Dev = require('../models/Dev')
 
 module.exports = {
+
 async	store(req, res) {
 		console.log(req.headers.user);
 		console.log(req.params.devId);
@@ -15,10 +16,14 @@ async	store(req, res) {
 			return res.status(400).json({ error: "Dev not exists" })
 		}
 
+		if (targetDev.likes.includes(loggedDev._id)) {
+			console.log("Deu Match")
+		}
+
 		loggedDev.likes.push(targetDev._id);
 
 		await loggedDev.save();
 
 		return res.json(loggedDev);
 	}
-};
+};// INDEX, SHOW, STORE, UPDATE , DELETE
